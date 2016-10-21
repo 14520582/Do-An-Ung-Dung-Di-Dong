2f1ccb06f08d6ca99bb3f491ad6188375a1c9c54
14520582 <bikinluacongtu@gmail.com>
2016-10-21 16:32:19 +0700

    v2

--- DoAn/coo/app/src/main/java/com/example/nghia/coo/MainActivity.java
+++ DoAn/coo/app/src/main/java/com/example/nghia/coo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nghia.coo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,14 +10,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     NavigationView navigationView = null;
     Toolbar toolbar = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+          if (AccessToken.getCurrentAccessToken() == null) {
+            goLoginScreen();
+
+        }
         setContentView(R.layout.activity_main);
         HomeFragment fragment = new HomeFragment();
         android.support.v4.app.FragmentTransaction fragmentTransaction =
@@ -40,20 +55,38 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+    //    Bundle b = getIntent().getExtras();
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+       TextView facebookName = (TextView) navigationView.getHeaderView(0).findViewById(R.id.username);
+     //   TextView Email = (TextView) navigationView.getHeaderView(0).findViewById(R.id.email);
+       // String a=new String(Profile.getCurrentProfile().getName());
+        //if(a!=null)
+        //facebookName.setText("asd");
+        //Email.setText(b.getCharSequence("email"));
+      /*  DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();*/
+
+
+
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
+
+    public void logout(View view) {
+        LoginManager.getInstance().logOut();
+        FirebaseAuth.getInstance().signOut();
+        goLoginScreen();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
